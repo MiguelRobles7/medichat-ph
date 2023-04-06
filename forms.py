691669a5c73932594809edb7ad6c958e015ffffb
Miguel Robles <donnaldmiguelrobles7@gmail.com>
2023-04-07 00:59:07 +0800

    Add general questions frontend

--- forms.py
+++ forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField, RadioField, BooleanField
 
+
 class GeneralForm(FlaskForm):
+    gender = StringField('Sex')
     bmi = IntegerField('BMI')
     age = IntegerField('Age')
-    submit_field =  SubmitField('Continue')
+    submit_field = SubmitField('CONTINUE >>')
+
 
 class SymptomsForm(FlaskForm):
-    symptoms = RadioField('Symptoms', choices = [('Yes', 'Yes'),('No', 'No')])
-    next_button =  SubmitField('Next')
+    symptoms = RadioField('Symptoms', choices=[('Yes', 'Yes'), ('No', 'No')])
+    next_button = SubmitField('Next')
